c2363ab92f6ceb5450bda81be0a5eb7d5235b762
RussianDaddy <sjoerd.weerstra@hotmail.com>
2017-03-08 18:49:14 +0100

    Extra scrape

---
+++ src/api/curl_html.php
@@ -0,0 +1,21 @@
+<?php
+
+include 'simple_html_dom.php';
+
+function download_html($href) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_URL, $href);
+    curl_setopt($curl, CURLOPT_REFERER, $href);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.125 Safari/533.4");
+    $str = curl_exec($curl);
+    curl_close($curl);
+
+    $dom = new simple_html_dom();
+	$dom->load($str, true, false);
+	
+    return $dom;
+}
